27f8d6a56b86dd27904ad5fea962b51264a18ab2
Tor N. Johnson <kasigi@gmail.com>
2023-08-16 11:45:12 +0300

    MERN-Stack Initial

---
+++ routes/api/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+
+// POST /api/users/login
+router.post('/login', usersCtrl.login);
+
+// POST /api/users
+router.post('/', usersCtrl.create);
+
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+
+
+module.exports = router;
